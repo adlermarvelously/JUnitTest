2912fa808148d6615548562d83db4d0e62bb126f
unknown <adlermerve@gmail.com>
2023-08-22 10:36:11 +0300

    first commit

---
+++ src/main/java/com/merveadler/Islemler.java
@@ -0,0 +1,15 @@
+package com.merveadler;
+
+public class Islemler {
+
+    public int topla(int a, int b) {
+        return a + b;
+    }
+
+    public double bolme(int a, int b) {
+        if(b==0)
+            throw new ArithmeticException("Bolen 0 olamaz");
+        return a / b;
+    }
+
+}
